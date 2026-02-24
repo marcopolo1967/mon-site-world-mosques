63d470d6e29f84bd0cd95af5f964f5c2812acc1f
marcopolo1967 <jack.meyers@yahoo.fr>
2026-02-24 05:50:00 +0100

    Désactivation temporaire de UPLOAD_PRESET

--- Mosquee_Annuaire/settings.py
+++ Mosquee_Annuaire/settings.py
@@ -278,7 +278,7 @@ CLOUDINARY_STORAGE = {
     'CLOUD_NAME': os.environ.get('CLOUDINARY_CLOUD_NAME'),
     'API_KEY': os.environ.get('CLOUDINARY_API_KEY'),
     'API_SECRET': os.environ.get('CLOUDINARY_API_SECRET'),
-    'UPLOAD_PRESET': os.environ.get('CLOUDINARY_UPLOAD_PRESET'),
+    #'UPLOAD_PRESET': os.environ.get('CLOUDINARY_UPLOAD_PRESET'),
 }
 
 # Définir Cloudinary comme storage par défaut
